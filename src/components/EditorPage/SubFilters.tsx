e4d0db00120059421b5386c12a804e70e1e8153f
Stanislav <srudenko2005@gmail.com>
2025-06-12 00:28:30 +1000

    -

--- src/components/EditorPage/SubFilters.tsx
+++ src/components/EditorPage/SubFilters.tsx
@@ -113,12 +113,12 @@ export default function SubFilters({
           response = await EFFECT_FUNCTIONS[activeFilter](
             imgSrc,
             activeHairStyle,
-            currentOption?.haircolor
+            currentOption
           );
         } else if (activeFilter === "hairstyle" && !activeHairStyle) {
           response = await EFFECT_FUNCTIONS[activeFilter](
             imgSrc,
-            currentOption?.hairstyle
+            currentOption
           );
         } else {
           const optionToApply =
