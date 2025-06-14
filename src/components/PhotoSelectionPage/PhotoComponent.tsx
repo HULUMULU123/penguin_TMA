f13cc8062c0f5cf7223e51d4da61e7aac73a7b69
Stanislav <srudenko2005@gmail.com>
2025-06-14 11:55:22 +1000

    check

--- src/components/PhotoSelectionPage/PhotoComponent.tsx
+++ src/components/PhotoSelectionPage/PhotoComponent.tsx
@@ -1,7 +1,50 @@
 // @ts-nocheck
 import React from "react";
-import { Image } from "./PhotoSelectionPage.styles";
+import styled from "styled-components";
+import check from "../../assets/icons/check.svg";
 import { useLongPress } from "use-long-press";
+
+const ImageWrapper = styled.div`
+  position: relative;
+  width: calc((100% - 2 * 3px) / 3);
+  margin-bottom: 0;
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  display: block;
+  user-select: none;
+`;
+
+const Overlay = styled.div`
+  pointer-events: none;
+  position: absolute;
+  inset: 0;
+  background-color: rgba(255, 255, 255, 0.2);
+`;
+
+const CheckCircle = styled.div`
+  pointer-events: none;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 40px;
+  height: 40px;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CheckImage = styled.img`
+  width: 24px;
+  height: 24px;
+  filter: brightness(0) invert(1); /* если нужно, чтобы сделать иконку белой */
+`;
 
 export default function PhotoComponent({
   img,
@@ -9,57 +52,37 @@ export default function PhotoComponent({
   setDeletePhotoId,
   setShowModalDelete,
 }) {
+  const [isPressed, setIsPressed] = useState(false);
+
   const longPressBind = useLongPress(
     () => {
-      setShowModalDelete(true);
-      console.log("long press");
+      setShowModalDelete(false);
       setDeletePhotoId(img.id);
+      setIsPressed(true);
     },
     {
       threshold: 600,
       captureEvent: true,
       cancelOnMovement: true,
+      onCancel: () => setIsPressed(false),
+      onFinish: () => setIsPressed(false),
     }
   );
 
   return (
-    <>
-      <Image
-        key={img.id}
+    <ImageWrapper {...longPressBind()}>
+      <StyledImage
         src={img.url}
         alt={`img-${img.id}`}
-        {...longPressBind()}
         onClick={() => handleClick(img.url)}
+        draggable={false}
       />
-      {/* {showModalDelete && (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            background: "rgba(0,0,0,0.5)",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            zIndex: 1000,
-          }}
-        >
-          <div
-            style={{
-              background: "#fff",
-              padding: 20,
-              borderRadius: 10,
-              minWidth: 200,
-            }}
-          >
-            <p>Удалить фото?</p>
-            <button onClick={() => handleDelete(deletePhotoId)}>Удалить</button>
-            <button onClick={() => setShowModalDelete(false)}>Отмена</button>
-          </div>
-        </div>
-      )} */}
-    </>
+      {isPressed && <Overlay />}
+      {isPressed && check && (
+        <CheckCircle>
+          <CheckImage src={check} alt="check icon" />
+        </CheckCircle>
+      )}
+    </ImageWrapper>
   );
 }
